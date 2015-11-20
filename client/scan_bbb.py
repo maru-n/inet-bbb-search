a94353c428a9d46d7eeebb19c4de45b55d776828
Maruyama Norihiro <norihiro.maruyama@gmail.com>
2015-11-20 15:47:56 +0900

    fix bug.

--- client/scan_bbb.py
+++ client/scan_bbb.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, DEVNULL
 import socket
 
 
@@ -12,21 +12,25 @@ def main():
     print(ip)
 
 def scan_bbb(search_ips, port):
+    ASYNC_NUM = 256
     available_ips = []
 
-    pipes = []
-    for ip in search_ips:
-        pipes.append(Popen(["ping", "-c", "1", "-t", "1", ip], bufsize=1024, stdout=PIPE))
+    search_ips_chunk_list = [search_ips[i:min(i+ASYNC_NUM, len(search_ips))] for i in range(0, len(search_ips), ASYNC_NUM)]
+    for search_ips_chunk in search_ips_chunk_list:
+        pipes = []
+        for ip in search_ips_chunk:
+            pipes.append(Popen(["ping", "-c", "1", "-t", "1", ip], bufsize=1024, stdout=DEVNULL))
 
-    for p, ip in zip(pipes, search_ips):
-        if(p.wait() == 0):
-            print('+', end='', flush=True)
-            available_ips.append(ip)
-        else:
-            print('.', end='', flush=True)
+        for p, ip in zip(pipes, search_ips):
+            p.wait()
+            if(p.returncode == 0):
+                print('+', end='', flush=True)
+                available_ips.append(ip)
+            else:
+                print('.', end='', flush=True)
     print()
 
-    found_ip = None
+    found_ip = ""
     for ip in available_ips:
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
